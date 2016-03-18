9bca00311290a6a79c8e88509d902a9bbdafe158
PacocaDev <alexandre.alp@live.com>
2016-03-18 01:36:49 -0300

    movement exercise

---
+++ Assets/controller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Movement (){
+		float speed = 1.9F;
+		float xtranslation = Input.GetAxis("Horizontal") * speed;
+		float ytranslation = Input.GetAxis("Vertical") * speed;
+
+		xtranslation *= Time.deltaTime;
+		ytranslation *= Time.deltaTime;
+
+
+		transform.Translate(xtranslation,ytranslation,0);
+        
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Movement();
+	}
+}
